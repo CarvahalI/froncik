8b76dfceabbf99c8600897d885055cfb9c0e1df7
Tomasz Witkowski <tommiewit@gmail.com>
2025-05-29 03:58:18 +0200

    dodanie backgroundu

--- src/App.tsx
+++ src/App.tsx
@@ -16,24 +16,29 @@ import i18n from "./i18n";
 
 function App() {
   return (
-      <BrowserRouter>
+    <div className="App">
+      <div className="App-content">
+        <BrowserRouter>
           <I18nextProvider i18n={i18n}>
-          <ApiProvider>
-      <Routes>
-          <Route path="/home" element={<HomePage/>}/>
-          <Route path="/login" element={<LoginForm/>}/>
-          <Route path="/books" element={<BooksList/>}/>
-          <Route path="/loans" element={<LoansList/>}/>
-          <Route path="/users" element={<UserList/>}/>
-          <Route path="/" element={<Navigate to={"/login"}/>}/>
-          <Route path="*" element={<h1>404</h1>} />
-          <Route path={"/homereader"} element={<HomeReader/>}/>
-          <Route path={"/bookslistreader"} element={<BooksListReader />}/>
-      </Routes>
-          </ApiProvider>
+            <ApiProvider>
+              <Routes>
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/books" element={<BooksList />} />
+                <Route path="/loans" element={<LoansList />} />
+                <Route path="/users" element={<UserList />} />
+                <Route path="/" element={<Navigate to={"/login"} />} />
+                <Route path="*" element={<h1>404</h1>} />
+                <Route path={"/homereader"} element={<HomeReader />} />
+                <Route path={"/bookslistreader"} element={<BooksListReader />} />
+              </Routes>
+            </ApiProvider>
           </I18nextProvider>
-      </BrowserRouter>
+        </BrowserRouter>
+      </div>
+    </div>
   );
 }
+
 
 export default App;
